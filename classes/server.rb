f880c2fc900da77fffe04c27dadf933c165def09
Dmitry Samoilov <dmitry-samoilov.l@yandex.ru>
2020-10-20 17:12:12 +0300

    Edited the Server class. Removed unnecessary methods, added the ability to set the port for the server.

--- classes/server.rb
+++ classes/server.rb
@@ -1,7 +1,7 @@
 class Server
   attr_reader :server
-  def initialize
-    @server = TCPServer.new 5000
+  def initialize(port = 5000)
+    @server = TCPServer.new port
   end
 
   def raise_session
@@ -25,22 +25,4 @@ class Server
     request.match(/\?cpu=(?<cpu>\d*),ram=(?<ram>\d*),hdd_type=(?<hdd_type>\w*),hdd_capacity=(?<hdd_capacity>\d*)/)
   end
 
-  def reveal_integers(arr)
-    arr.map { |val| val.match?(/\A\d+\z/) ? val.to_i : val }
-  end
-
-  def cost(pc_params)
-    price = CSV.open('02_ruby_prices.csv').map { |item| reveal_integers(item) }
-
-    cpu_price      = price[0][1]
-    ram_price      = price[1][1]
-    hdd_type_price = price.detect{ |price| price[0] == pc_params[:hdd_type] }.last
-
-    cost = pc_params[:cpu].to_i          * cpu_price +
-           pc_params[:ram].to_i          * ram_price +
-           pc_params[:hdd_capacity].to_i * hdd_type_price
-
-    # Переводим цену в рубли
-    cost = cost / 100
-  end
 end
