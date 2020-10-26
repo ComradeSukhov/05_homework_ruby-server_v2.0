ac8d868e94b2d2cc1e73506068812603a337fd32
Dmitry Samoilov <dmitry-samoilov.l@yandex.ru>
2020-10-26 21:22:47 +0300

    Edited server class

--- classes/server.rb
+++ classes/server.rb
@@ -1,5 +1,9 @@
+require 'socket'
+require 'json'
+
 class Server
   attr_reader :server
+
   def initialize(port = 5000)
     @server = TCPServer.new port
   end
@@ -8,21 +12,14 @@ class Server
     while session = server.accept
 
     request   = session.gets
-    pc_params = to_parse_pc_params(request)
+    cost      = ResponseController.new(request).result
 
     session.print "HTTP/1.1 200\r\n"
-    session.print "Content-Type: text/html\r\n"
+    session.print "Content-Type: application/json\r\n"
     session.print "\r\n"
-    session.print "Цена ВМ составляет: #{cost(pc_params)} руб."
+    session.print({cost: cost}.to_json)
 
     session.close
     end
   end
-
-  private
-
-  def to_parse_pc_params(request)
-    request.match(/\?cpu=(?<cpu>\d*),ram=(?<ram>\d*),hdd_type=(?<hdd_type>\w*),hdd_capacity=(?<hdd_capacity>\d*)/)
-  end
-
 end
